88f5d86c15d32a87c4144cf07b44afaf32b0c64b
Savio <savio.96@hotmail.com>
2023-03-11 16:56:30 -0300

    feat: Adicionado a carteira do usuário

--- src/Components/ConnectWallet/ConnectWallet.tsx
+++ src/Components/ConnectWallet/ConnectWallet.tsx
@@ -2,19 +2,45 @@ import React, { useState, useEffect, createContext, useContext } from "react";
 import styles from "./connectwallet.module.scss";
 import classnames from "classnames";
 import { UserProvider, UserContext } from "../UserInfo/UserInfo";
+import { Signer } from "@waves/signer";
+import { ProviderKeeper, isKeeperInstalled } from "@waves/provider-keeper";
+import { consoleLoggerLevel } from "vtex/lib/api/logger";
 
 const ConnectWallet = () => {
   const { wallet, changeWallet, privateKey, changePrivateKey } =
     useContext(UserContext);
 
-  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleOnClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    console.log("clicou");
-    let valor = wallet == "3PP" ? "Conectar" : "3PP";
+    const authData = { data: "Auth on my site" };
+    KeeperWallet.auth(authData)
+      .then((auth) => {
+        console.log(auth); //displaying the result on the console
+        /*...processing data */
+      })
+      .catch((error) => {
+        console.error(error); // displaying the result on the console
+        /*...processing errors */
+      }); /*
+    const isInstalled = await isKeeperInstalled();
+    const signer = new Signer({
+      // Specify URL of the node on Testnet
+      NODE_URL: "https://nodes-testnet.wavesnodes.com",
+    });
+    const keeper = new ProviderKeeper();
+    signer.setProvider(keeper);
+    const user = await signer.login();
+    console.log(user["address"]);
+    if (isInstalled) {
+      console.log("Connected");
+    }
+    
+    let balance =
+      await `https://nodes-testnet.wavesnodes.com/addresses/balance/${wallet}`;*/
+    let valor = wallet == wallet ? "Conectar" : wallet;
     let valorPriveteKey = privateKey == "111" ? "" : "111";
     changePrivateKey(valorPriveteKey);
     changeWallet(valor);
-    console.log(wallet);
   };
   return (
     <>
