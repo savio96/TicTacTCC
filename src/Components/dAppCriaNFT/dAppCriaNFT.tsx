971ee42b6cfefbff12896f192ed2d9b6363b0dc1
Leonardo <leo-m-f1@hotmail.com>
2023-03-29 18:03:55 -0300

    fix: Add button

--- src/Components/dAppCriaNFT/dAppCriaNFT.tsx
+++ src/Components/dAppCriaNFT/dAppCriaNFT.tsx
@@ -7,12 +7,19 @@ import styles from "./dAppCriaNFT.module.scss";
 const DAppCriaNFT = () => {  
   
   
+  const resgate = (e: React.MouseEvent<HTMLButtonElement>) => {
+  };
 
 
-  
+
+
+
+
+
   return (
     <div>
-      <button type="submit" className={classnames(styles["btn-nft"], "btn", "btn-primary")}>Resgatar!</button>
+      <button type="submit" className={classnames(styles["btn-nft"], "btn", "btn-primary")} onClick = {resgate}>Resgatar!</button>
+    
     </div>
   );
 };
