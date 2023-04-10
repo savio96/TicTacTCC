273aca10a9f25f483b92d242063a553d0f06174f
Savio <savio.96@hotmail.com>
2023-04-10 15:15:56 -0300

    feat: Adicionado o botao de deposito e faucet

--- src/Pages/Teste/Teste.tsx
+++ src/Pages/Teste/Teste.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { BtnClaimCoin, BtnDepositCoin } from "../../Components/btnDapp/btnDapp";
 const Teste = () => {
-  return <></>;
+  return (
+    <>
+      <div>
+        <BtnClaimCoin></BtnClaimCoin>
+        <BtnDepositCoin></BtnDepositCoin>
+      </div>
+    </>
+  );
 };
 
 export { Teste };
