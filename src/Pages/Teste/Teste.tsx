bc9a001e39446f4d898295dc697a934a666653c0
Savio <savio.96@hotmail.com>
2023-04-16 20:40:02 -0300

    refactor: Limpeza de comentarios no codigo

--- src/Pages/Teste/Teste.tsx
+++ src/Pages/Teste/Teste.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { BtnClaimCoin, BtnDepositCoin } from "../../Components/btnDapp/btnDapp";
+
 const Teste = () => {
   return (
     <>
-      <div>
-        <BtnClaimCoin></BtnClaimCoin>
-        <BtnDepositCoin></BtnDepositCoin>
-      </div>
+      <div></div>
     </>
   );
 };
