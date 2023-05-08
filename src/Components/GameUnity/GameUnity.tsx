bb562bbd438c876628295d59fdb6b0cd043711b7
Savio <savio.96@hotmail.com>
2023-05-08 18:07:39 -0300

    feat: Configurado comunicação c# com react

--- src/Components/GameUnity/GameUnity.tsx
+++ src/Components/GameUnity/GameUnity.tsx
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useContext, useEffect, useCallback } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import styles from "./gameunity.module.scss";
 import classnames from "classnames";
+import { UserContext } from "../UserInfo/UserInfo";
 
 function GameUnity() {
-  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
-    loaderUrl: "/assets/jogo/Build/gameTic.loader.js",
-    dataUrl: "/assets/jogo/Build/gameTic.data",
-    frameworkUrl: "/assets/jogo/Build/gameTic.framework.js",
-    codeUrl: "/assets/jogo/Build/gameTic.wasm",
+  let { wallet, changeWallet } = useContext(UserContext);
+
+  const {
+    unityProvider,
+    isLoaded,
+    loadingProgression,
+    addEventListener,
+    removeEventListener,
+    sendMessage,
+  } = useUnityContext({
+    loaderUrl: "/assets/jogo/Build/jogohtml.loader.js",
+    dataUrl: "/assets/jogo/Build/jogohtml.data",
+    frameworkUrl: "/assets/jogo/Build/jogohtml.framework.js",
+    codeUrl: "/assets/jogo/Build/jogohtml.wasm",
   });
 
+  const handleEndMatch = useCallback(
+    (
+      criador: any,
+      convidado: any,
+      solucoesArr: any,
+      boardArr: any,
+      tam: any
+    ) => {
+      console.log(criador);
+      console.log(convidado);
+      console.log(solucoesArr);
+      console.log(boardArr);
+      console.log(tam);
+    },
+    []
+  );
+
+  useEffect(() => {
+    addEventListener("endMatch", handleEndMatch);
+    return () => {
+      removeEventListener("endMatch", handleEndMatch);
+    };
+  }, [addEventListener, removeEventListener, handleEndMatch]);
+
+  function enviaCarteira() {
+    sendMessage("SalaDeEntrada", "getWallet", wallet);
+  }
   // We'll round the loading progression to a whole number to represent the
   // percentage of the Unity Application that has loaded.
   const loadingPercentage = Math.round(loadingProgression * 100);
-
+  enviaCarteira();
   return (
     <div className={classnames(styles["container"])}>
       {isLoaded === false && (
