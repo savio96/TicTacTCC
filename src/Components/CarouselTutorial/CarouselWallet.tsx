9f2387fb7340d8c117b4c6a771c1d291adda6616
Savio <savio.96@hotmail.com>
2023-06-13 21:52:13 -0300

    fix: Corrigido o carousel tutorial paddings

--- src/Components/CarouselTutorial/CarouselWallet.tsx
+++ src/Components/CarouselTutorial/CarouselWallet.tsx
@@ -28,7 +28,7 @@ export default class CarouselWallet extends Component {
     };
     return (
       <div className={styles["container"]}>
-        <h2> Single Item</h2>
+        <h2> Tutorial da Criação da Carteira</h2>
         <Slider {...settings}>
           <div className={styles["slides"]}>
             <img src={titulo} alt="Titulo Slide" />
@@ -37,20 +37,105 @@ export default class CarouselWallet extends Component {
             <img src={passo1} alt="Passo 1" />
             <div className={styles["slides-text"]}>
               <h3>Passo 1</h3>
-              <p>Abra seu google chrome e pesquise por "Chrome Web Store"</p>
+              <p>Abra seu google chrome e pesquise por "Chrome Web Store".</p>
             </div>
           </div>
-          <div>
-            <h3>3</h3>
+          <div className={styles["slides"]}>
+            <img src={passo2} alt="Passo 2" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 2</h3>
+              <p>No Chrome Web Store, pesquise por "Keeper Wallet".</p>
+            </div>
           </div>
-          <div>
-            <h3>4</h3>
+          <div className={styles["slides"]}>
+            <img src={passo3} alt="Passo 3" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 3</h3>
+              <p>Abra a extensão Kepper Wallet.</p>
+            </div>
           </div>
-          <div>
-            <h3>5</h3>
+          <div className={styles["slides"]}>
+            <img src={passo4} alt="Passo 4" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 4</h3>
+              <p>Clique em "Usar no Chrome".</p>
+            </div>
           </div>
-          <div>
-            <h3>6</h3>
+          <div className={styles["slides"]}>
+            <img src={passo5} alt="Passo 5" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 5</h3>
+              <p>
+                Clique em "Adicionar extensão" e ela irá ser adicionada ao seu
+                Chrome.
+              </p>
+            </div>
+          </div>
+          <div className={styles["slides"]}>
+            <img src={passo6} alt="Passo 6" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 6</h3>
+              <p>
+                Vá no seu gerenciamento de extensões e clique em "Keeper
+                Wallet".
+              </p>
+            </div>
+          </div>
+          <div className={styles["slides"]}>
+            <img src={passo7} alt="Passo 7" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 7</h3>
+              <p>Irá abrir uma nova aba e clique em "iniciar"</p>
+            </div>
+          </div>
+          <div className={styles["slides"]}>
+            <img src={passo8} alt="Passo 8" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 8</h3>
+              <p>
+                Crie uma senha que será armazenada localmente, aceite os termos
+                e clique em "Continuar".
+              </p>
+            </div>
+          </div>
+          <div className={styles["slides"]}>
+            <img src={passo9} alt="Passo 9" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 9</h3>
+              <p>
+                Antes de clicar em "Criar uma conta nova", selecione a rede
+                "Testnet" no lugar de "Mainnet" no canto esquerdo inferior e
+                logo em seguida escolha um avatar e clique em "Continuar".
+              </p>
+            </div>
+          </div>
+          <div className={styles["slides"]}>
+            <img src={passo10} alt="Passo 10" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 10</h3>
+              <p>
+                Salve de uma forma segura sua cadeia de palavras aleatórias,
+                clique em "Continuar". Na próxima tela, selecione sua senha na
+                ordem correta e clique em "Confirmar".
+              </p>
+            </div>
+          </div>
+          <div className={styles["slides"]}>
+            <img src={passo11} alt="Passo 11" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 11</h3>
+              <p>
+                Crie um nome da conta, clique em "Continuar" e finalize o
+                processo.
+              </p>
+            </div>
+          </div>
+          <div className={styles["slides"]}>
+            <img src={passo12} alt="Passo 12" />
+            <div className={styles["slides-text"]}>
+              <h3>Passo 12</h3>
+              <p>Agora pela extensão, você já tem acesso a sua carteira.</p>
+            </div>
           </div>
         </Slider>
       </div>
